8eed4154f57805fa2a8acf3e58df85d741f837c8
yagray <yagray@mail.ru>
2019-06-14 14:12:55 +0300

    code

---
+++ app/src/main/java/k/s/yarlykov/familychat/MainActivity.kt
@@ -0,0 +1,12 @@
+package k.s.yarlykov.familychat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
